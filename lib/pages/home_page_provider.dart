20e737e76027ce49421596f2983dc69ad6152d5e
MayaZand <mayaz96101@gmail.com>
2024-02-07 12:52:52 +0200

    Fixing display buttons, sorting posts by category, updating the login screens.

--- lib/pages/home_page_provider.dart
+++ lib/pages/home_page_provider.dart
@@ -12,20 +12,19 @@ class HomePageProvider extends StatefulWidget {
 
 class HomePageProviderState extends State<HomePageProvider> {
   String selectedLocation = "All";
+  String selectedCategory = "All";
   int originalVisiblePostCount = 10;
   int visiblePostCount = 10;
   bool _isLoading = true;
 
   List<ServiceCall?> allCalls = [];
 
-  // set state when build the page
   @override
   void initState() {
     super.initState();
     fetchCalls();
   }
 
-  // async function to fetch all service calls
   Future<void> fetchCalls() async {
     try {
       List<ServiceCall?> calls = await ServiceCall.getAllPosts();
@@ -43,75 +42,81 @@ class HomePageProviderState extends State<HomePageProvider> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // top bar
       appBar: AppBar(
         title: Text("Posts"),
       ),
-
-      // body
-      body:
-          // check if all data have fetched, else show progress bar
-          _isLoading
-              ? Center(child: CircularProgressIndicator())
-              : Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(16),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          // location filter
-                          Text("Location: "),
-                          DropdownButton<String>(
-                            value: selectedLocation,
-                            items: getUniqueLocations().map((location) {
-                              return DropdownMenuItem<String>(
-                                value: location,
-                                child: Text(location),
-                              );
-                            }).toList(),
-                            onChanged: (value) {
-                              setState(() {
-                                selectedLocation = value!;
-                                visiblePostCount = originalVisiblePostCount;
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                    // create calls feed
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: allCalls.length,
-                        itemBuilder: (context, index) {
-                          // Check if the call's location matches the selected location
-                          if (selectedLocation == "All" ||
-                              allCalls[index]?.area == selectedLocation) {
-                            return CallCard(call: allCalls[index]);
-                          } else {
-                            // Return an empty container if the location doesn't match
-                            return Container();
-                          }
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Location: "),
+                      DropdownButton<String>(
+                        value: selectedLocation,
+                        items: getUniqueLocations().map((location) {
+                          return DropdownMenuItem<String>(
+                            value: location,
+                            child: Text(location),
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            selectedLocation = value!;
+                            visiblePostCount = originalVisiblePostCount;
+                          });
                         },
                       ),
-                    ),
-                  ],
+                      SizedBox(width: 20),
+                      Text("Category: "),
+                      DropdownButton<String>(
+                        value: selectedCategory,
+                        items: getUniqueCategories().map((category) {
+                          return DropdownMenuItem<String>(
+                            value: category,
+                            child: Text(category),
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            selectedCategory = value!;
+                            visiblePostCount = originalVisiblePostCount;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-      // bottom bar
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: allCalls.length,
+                    itemBuilder: (context, index) {
+                      if ((selectedLocation == "All" ||
+                              allCalls[index]?.area == selectedLocation) &&
+                          (selectedCategory == "All" ||
+                              allCalls[index]?.category == selectedCategory)) {
+                        return CallCard(call: allCalls[index]);
+                      } else {
+                        return Container();
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
-          // home button
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
           ),
-          // profile button
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
             label: 'Profile',
           ),
-          // history button
           BottomNavigationBarItem(
             icon: Icon(Icons.history),
             label: 'History',
@@ -120,20 +125,16 @@ class HomePageProviderState extends State<HomePageProvider> {
         onTap: (index) {
           switch (index) {
             case 0:
-              // Handle tapping on the Home button if needed
               break;
             case 1:
-              // Handle tapping on the Profile button
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) =>
-                      ProviderProfile(), // Replace with the actual ProfileProvider widget
+                  builder: (context) => ProviderProfile(),
                 ),
               );
               break;
             case 2:
-              // Handle tapping on the History button if needed
               break;
           }
         },
@@ -141,14 +142,21 @@ class HomePageProviderState extends State<HomePageProvider> {
     );
   }
 
-  // generate locations list from the allcalls
   List<String> getUniqueLocations() {
     Set<String> uniqueLocations = Set<String>();
     uniqueLocations.add("All");
     for (var call in allCalls) {
       uniqueLocations.add(call?.area ?? '');
     }
-
     return uniqueLocations.toList();
+  }
+
+  List<String> getUniqueCategories() {
+    Set<String> uniqueCategories = Set<String>();
+    uniqueCategories.add("All");
+    for (var call in allCalls) {
+      uniqueCategories.add(call?.category ?? '');
+    }
+    return uniqueCategories.toList();
   }
 }
